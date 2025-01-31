3ccf533ae8ef5bcc7cfc122b389ad269c5bdca24
Artem Dragunov <dragunovartem99@gmail.com>
2025-01-31 15:51:44 +0300

    Add basic implementation

--- src/components/PgnViewer.vue
+++ src/components/PgnViewer.vue
@@ -1,2 +1,18 @@
-<script setup lang="ts"></script>
-<template><div></div></template>
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import LichessPgnViewer from "lichess-pgn-viewer";
+
+const board = ref<HTMLElement | null>(null);
+
+onMounted(() => {
+	if (!board.value) {
+		throw new Error("Can't mount the PGN viewer");
+	}
+
+	LichessPgnViewer(board.value, {});
+});
+</script>
+
+<template>
+	<div ref="board"></div>
+</template>
