07ff3242b5f77edb2a0c4a580fda646f97b14908
Artem Dragunov <dragunovartem99@gmail.com>
2025-01-31 22:46:41 +0300

    Use shorter syntax

--- src/components/PgnViewer.vue
+++ src/components/PgnViewer.vue
@@ -3,32 +3,16 @@ import { ref, onMounted } from "vue";
 import { VuePgnViewer } from "../objects/VuePgnViewer";
 import type { PgnViewerApi, PgnViewerConfig } from "../types";
 
-const props = defineProps<{
-	config?: PgnViewerConfig;
-}>();
-
-const emit = defineEmits<{
-	(e: "ready", api: PgnViewerApi): void;
-}>();
-
+const props = defineProps<{ config?: PgnViewerConfig }>();
+const emit = defineEmits<{ (e: "ready", api: PgnViewerApi): void }>();
 const board = ref<HTMLDivElement | null>(null);
+
 const viewer = new VuePgnViewer(props.config || {});
 
-function mountPgnViewer() {
-	if (!board.value) {
-		throw new Error("Can't mount the PGN viewer");
-	}
-	viewer.mount(board.value);
-}
+const mountPgnViewer = () => board.value && viewer.mount(board.value);
+const exposeApi = () => viewer.api && emit("ready", viewer.api);
 
-function exposeApi() {
-	viewer.api && emit("ready", viewer.api);
-}
-
-onMounted(() => {
-	mountPgnViewer();
-	exposeApi();
-});
+onMounted(() => mountPgnViewer() ?? exposeApi());
 </script>
 
 <template>
