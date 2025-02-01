5e6c6db6c6a32b1466e340cd7090ae3e99a74232
Artem Dragunov <dragunovartem99@gmail.com>
2025-02-01 03:32:45 +0300

    Set base explicitly

--- web.config.ts
+++ web.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
+	base: "https://dragunovartem99.github.io/vue-pgn-viewer/",
 	build: { outDir: "web" },
 	plugins: [vue()],
 });
