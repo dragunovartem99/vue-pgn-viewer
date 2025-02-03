a77fa2e32ad03ef823249fcea0b2e231044adf4a
Artem Dragunov <dragunovartem99@gmail.com>
2025-02-03 16:21:29 +0300

    Add simplest fields and methods

--- src/types/pgn-viewer-api.ts
+++ src/types/pgn-viewer-api.ts
@@ -1,1 +1,27 @@
-export type PgnViewerApi = any;
+// https://github.com/lichess-org/pgn-viewer/blob/master/src/pgnViewer.ts
+
+import type { Api as ChessgroundApi } from "chessground/api";
+import type { Config as ChessgroundConfig } from "chessground/config";
+
+export type PgnViewerApi = {
+	ground: ChessgroundApi;
+	div?: HTMLElement;
+	flipped: boolean;
+	pane: "board" | "menu" | "pgn";
+	autoScrollRequested: boolean;
+
+	focus(): void;
+
+	orientation(): string;
+	flip(): void;
+
+	toggleMenu(): void;
+	togglePgn(): void;
+
+	cgState(): ChessgroundConfig;
+
+	analysisUrl(): string;
+	practiceUrl(): string;
+
+	setGround(api: ChessgroundApi): void;
+};
